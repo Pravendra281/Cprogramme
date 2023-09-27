dbfa8a120f58342ed439c474a952666962474943
Pravendra <pravendrasingh281@gmail.com>
2023-09-27 23:41:19 +0530

    new star pattern in C

---
+++ starPattern/numberPattern2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j,k=0,x;
+    for (i=1;i<=9;i++){
+       i<6?k++:k--;
+       x=1;
+        for (j=1;j<=5;j++){
+            if(j>=6-k)
+            {
+               printf("%d",x);
+               x++;
+            }
+            else
+            {
+                printf(" ");
+            
+            }
+            
+        }printf("\n");
+      
+    }  printf("\n");
+        return 0;
+}
