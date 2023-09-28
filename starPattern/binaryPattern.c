472588caebac1dd7a87de9779586399e3bc02c9c
Pravendra <pravendrasingh281@gmail.com>
2023-09-28 23:50:17 +0530

    newstarPattern in c

---
+++ starPattern/binaryPattern.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j,k;
+    for (i=1;i<=6;i++){
+      k=1;
+        for (j=1;j<=6;j++){
+            if(j<=i)
+            {
+           printf("%d",k);
+            k=1-k;
+            }
+            else
+            {
+                printf(" ");
+            
+            }
+            
+        }printf("\n");
+      
+    }  printf("\n");
+        return 0;
+}
