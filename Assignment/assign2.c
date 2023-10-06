9a9485387509f248706b20d1d7d41248ce01a632
Pravendra <pravendrasingh281@gmail.com>
2023-10-06 19:21:50 +0530

    Assignment2,3

---
+++ Assignment/assign2.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<conio.h>
+
+//Q1) int main()
+// {
+//     int x,y,z;
+//     float avg;
+//     printf("Enter three Numbers");
+//     scanf("%d%d%d",&x,&y,&z);
+//     avg=(x+y+z)/3.0;
+//     printf("Avg is %f",avg);
+// }
+
+//Q2) int main()
+// {
+//     int r;
+//     float cir;
+//    printf("Enter radius of circle");
+//    scanf("%d",&r);
+//    cir=2*3.14*r;
+//    printf("Circumference of circle is %f",cir);
+// }
+// Q3 int main()
+// {
+//     int P,R,T;
+//     float si;
+//     printf("Enter value of P R T");
+//     scanf("%d%d%d",&P,&R,&T);
+//     si=(P*R*T)/100.0;
+//     printf("Simple Interest is %f",si);
+
+//  }
+// Q4 int main()
+// {
+//     int a;
+//     float vc;
+//     printf("Enter value of a");
+//     scanf("%d",&a);
+//    vc=a*a*a;
+//     printf("Volume of Cuboid is %f",vc);
+
+//  }
+
+ int main()
+{
+    int cp,sp;
+    float profit;
+    printf("Enter value of cp and sp");
+    scanf("%d%d",&cp,&sp);
+   profit=sp-cp;
+    printf("Profit is %f",profit);
+
+ }
---
+++ Assignment/assign3.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<conio.h>
+
+// Q1 int main()
+// {
+//     char c;
+//     printf("Enter a character");
+//     scanf("%c",&c);
+//     printf("%c%d",c,c);
+//     return 0;
+
+// }
+
+//  Q2 int main()
+// {
+//     int c;
+//     printf("Enter a Number");
+//     scanf("%d",&c);
+//     printf("%d %c",c,c);
+//     return 0;
+
+// }
+// Q3 int main()
+// {
+//     int c;
+//     printf("Enter three character");
+//     scanf("%d%d%d",&c,&c,&c);
+//     printf("%d%c%d%c%d%c",c,c,c);
+//     return 0;
+
+// }
+// Q4 int main()
+// {
+//     int x,y;
+
+//     printf("Enter a Number");
+//     scanf("%d",&x);
+//     y=x%10;
+//     printf("%d",y);
+//     return 0;
+
+// }
+int main()
+{
+    int x,y;
+
+    printf("Enter a Number");
+    scanf("%d",&x);
+    y=x/10;
+    printf("%d",y);
+    return 0;
+
+}
